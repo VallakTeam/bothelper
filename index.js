400e52ec4621fefdcc4fb4a89cbaaaaa6148928e
Glitch (decorous-sagittarius) <none>
2018-08-20 04:51:48 +0000

    😺🐛 Checkpoint ./index.js:1299617/309 ./package.json:1299617/25 ./watch.json:1299617/2

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ music.commands = new Discord.Collection();
 const {color} = require('./config.json');
 const queue = new Map();
 
-
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log('Pinging');
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
 
 
 music.on('message', async message => {
