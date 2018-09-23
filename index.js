d3619f26c73119e880afdb2707437bf0b9907951
Glitch (loud-lynx) <none>
2018-09-23 08:15:19 +0000

    👾🌶 Checkpoint ./index.js:1299617/7567 ./watch.json:1299617/5 ./package.json:1299617/2700

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const Discord = require("discord.js");
 const music = new Discord.Client({disableEveryone: true});
 music.commands = new Discord.Collection();
+const superagent = require("superagent");
 const {color} = require('./config.json');
 const queue = new Map();
+const { Canvas } = require("canvas-constructor"); // You can't make images without this.
+const { resolve, join } = require("path"); // This is to get a font file.
+const { Attachment } = require("discord.js"); // This is to send the image via discord.
+const { get } = require("snekfetch");
 
 const http = require('http');
+const fs = require("fs");
 const express = require('express');
 const app = express();
 app.get("/", (request, response) => {
@@ -45,4 +51,30 @@ music.user.setActivity('WELCOMER GOOD GAME!!')
     console.log(`Logged in as : ${music.user.tag}`);
     console.log(`${music.user.username} is ready!`)
               
+});
+music.on("guildMemberAdd", async member => {  
+            var namam = member.user.username
+            var jadim = namam.length > 12 ? namam.substring(0, 10) + "..." : namam;
+            async function createCanvas() {
+            var imageUrlRegex = /\?size=2048$/g;
+
+            var {body: background} = await superagent.get(`https://cdn.discordapp.com/attachments/490474632147763200/491921782220128287/Screenshot_33.png`);
+            var {body: avatar} = await superagent.get(member.user.displayAvatarURL.replace(imageUrlRegex, "?size=128"));
+
+            return new Canvas(856, 376)
+              .addImage(avatar, 100, 50, 256, 256, 128)
+      .setColor('#11c40f')
+              .setTextAlign('left')
+              .setTextFont('bold 30px Arial')
+              .addImage(background, 0, 0, 856, 376)
+              .addText(`${jadim}`, 295, 88)
+              .addImage(avatar, 40, 63, 164, 140)
+              .toBufferAsync();
+            }
+    let channel = member.guild.channels.get(`493031211321655297`);
+  channel.send(`💞 **Selamat Datang** ${member.user} **Jangan lupa untuk melihat <#493044741798363136> terlebih dahulu. Isi biodatanya ya biar saling kenal <#493044916801503233> semoga betah**💕`,{
+  files: [{
+    attachment: await createCanvas(),
+    name: 'MyID Welcome.jpg'
+  }] })
 });
