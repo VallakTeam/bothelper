5bdaed60c7a513db05afa5ec4bed9a614dbfce10
Glitch (loud-lynx) <none>
2019-06-03 21:49:18 +0000

    💄🗻 Checkpoint ./commands/loop.js:1299617/23

--- commands/loop.js
+++ commands/loop.js
@@ -4,10 +4,10 @@ exports.run = async(music, message, args, color, queue) => {
 const serverQueue = queue.get(message.guild.id);  
   const loop = new Discord.RichEmbed()
   .setDescription("🔁 **Enable**")
-  .setColor("#00000")
+  .setColor("#0e123a")
   const unloop = new Discord.RichEmbed()
   .setDescription("🔁 **Disable**")
-  .setColor("#0aaaeb")
+  .setColor("#0e123a")
   serverQueue.loop = !serverQueue.loop;
   if(serverQueue.loop) return message.channel.send(loop)
   return message.channel.send(unloop)
