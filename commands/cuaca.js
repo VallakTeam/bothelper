f5bb192216af763d74633fb460b161db4ad5b641
Glitch (verter-music) <none>
2020-01-01 22:12:47 +0000

    🎁🔮 Checkpoint ./commands/cuaca.js:6470734/3618 ./commands/anime.js:6470734/1673 ./commands/Google.js:6470734/1212

---
+++ commands/cuaca.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+const { stripIndents } = require("common-tags");
+const fetch = require("node-fetch");
+
+exports.run = async (client, message, args) => {
+        const daerah = args.join(" ");
+  
+        if (!daerah) {
+            return message.channel.send("Usage: /cuaca <kota>")
+                .then(m => m.delete(5000));
+        }
+
+        const url = `https://rest.farzain.com/api/cuaca.php?id=${daerah}&apikey=21AuixDfnVIOkjlCOlfVcZJQe`;
+        
+        let res; 
+
+        try {
+            res = await fetch(url).then(url => url.json());
+        } catch (e) {
+            return message.reply("Tidak Menemukan Cuaca Di Kota Itu")
+                .then(m => m.delete(5000));
+        }
+
+        const cuaca = res.respon;
+
+        const embed = new Discord.RichEmbed()
+            .setColor("RANDOM")
+            .addField(`Daftar Cuaca`, stripIndents`**- Tempat:** ${cuaca.tempat}
+            **- Cuaca:** ${cuaca.cuaca}
+            **- Deskripsi:** ${cuaca.deskripsi}
+            **- Suhu:** ${cuaca.suhu}
+            **- Kelembapan:** ${cuaca.kelembapan}
+            **- Udara:** ${cuaca.udara}
+            **- Angin:** ${cuaca.angin}`)
+        message.channel.send(embed);
+
+
+}
