ea7a24dd5d10772d77a295dfba677f99274340ea
Glitch (verter-music) <none>
2020-01-02 19:34:18 +0000

    🍨💠 Checkpoint ./commands/help.js:6470734/2891 ./watch.jsons:6470734/2 ./commands/radio.js:6470734/23 ./commands/statistics.js:6470734/193

--- commands/help.js
+++ commands/help.js
@@ -3,3 +3,21 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   const embed = new Discord.RichEmbed()
 
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+  let embed = new Discord.RichEmbed()
+    .setTitle("help Commands")
+  .setAuthor(`${bot.user.tag}`, bot.user.displayAvatarURL)
+    .setThumbnail(bot.user.displayAvatarURL)
+    .addField("r.main","Show a pokemon information \n Either enter the french or english name")
+    .addField("r.","Show a pokemon information \n Either enter the french or english name")
+    .addField("r.main","Show a pokemon information \n Either enter the french or english name")
+  .setColor("#e2df1b")
+    .setFooter("if you want to suggest us something you can do it by doing ?suggest <suggestion>")
+    .setTimestamp()
+    message.channel.send(embed);
+}
+module.exports.help = {
+    name: "help"
+}
