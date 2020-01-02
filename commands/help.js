0ee91cfebc626dfddd06a36492c4ce2a9d4768ab
Glitch (verter-music) <none>
2020-01-02 20:33:12 +0000

    🚴🐾 Checkpoint ./commands/help.js:6470734/323

--- commands/help.js
+++ commands/help.js
@@ -3,8 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
 let commands = new Discord.RichEmbed()
+let avatar = client
 .setAuthor(`Rakan Aditya`)
-.addField(`---- help command --- `,`\`r.main\ `,`\`r.mod\ `,`\`r.time\ `,`\`r.statistics\``)
+.addField(`---- help command --- `,`\`r.main\`  \`r.mod\` \`r.time\` \`r.statistics\``)
+.addField(`---- bot invite --- `,`[\`klik disini\`](https://discordapp.com/oauth2/authorize?client_id=657351969253490699&permissions=8&scope=bot)`)
 
 message.channel.send(commands).then(message => message.react('✔'))
 
