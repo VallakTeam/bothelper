ec6abd9debd83336834df3677a09a17a95a204b6
Glitch (verter-music) <none>
2020-01-02 20:45:11 +0000

    🔮🏥 Checkpoint ./commands/help.js:6470734/1441

--- commands/help.js
+++ commands/help.js
@@ -3,8 +3,11 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
 let commands = new Discord.RichEmbed()
-let avatar = client
-.setAuthor(`Rakan Aditya`)
+  
+  .setColor("#4290AE")
+  .setTimestamp()
+  .setAuthor(`Help List Rakan Aditya™`, "https://discordapp.com/channels/609377474815721472/662300978845974549/662395415022403586")
+  .setThumbnail("https://discordapp.com/channels/609377474815721472/662300978845974549/662395415022403586")
 .addField(`---- help command --- `,`\`r.main\`  \`r.mod\` \`r.time\` \`r.statistics\``)
 .addField(`---- bot invite --- `,`[\`klik disini\`](https://discordapp.com/oauth2/authorize?client_id=657351969253490699&permissions=8&scope=bot)`)
 
