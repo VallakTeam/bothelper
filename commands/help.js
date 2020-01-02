aa981839d5c39d37fb804245700006fed6cb2769
Glitch (verter-music) <none>
2020-01-02 20:21:12 +0000

    👨😺 Checkpoint ./commands/help.js:6470734/790 ./watch.jsons:6470734/2

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
 
 let commands = new Discord.RichEmbed()
 .setAuthor(`Rakan Aditya`)
-.addField(`---- main command --- `,`\`r.main\``)
+.addField(`---- help command --- `,`\`r.main\ `,`\`r.mod\ `,`\`r.time\ `,`\`r.statistics\``)
 
 message.channel.send(commands).then(message => message.react('✔'))
 
