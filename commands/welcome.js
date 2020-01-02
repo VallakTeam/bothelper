4ad2b30f126189bceafb090d0770c1050012ef55
Glitch (verter-music) <none>
2020-01-02 23:32:47 +0000

    🍴🌕 Checkpoint ./commands/welcome.js:6470734/1176

--- commands/welcome.js
+++ commands/welcome.js
@@ -1,22 +1,17 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+bot.on("guildMemberAdd", member => {
+  const channel = member.guild.channels
+    .find(ch => ch.id === "662433738981900338");
+  if (!channel) return;
+  channel.send(
+    `**Welcome To Server .....**`
+  );
+}); 
 
-const newUsers = [];
-
-
-client.on("guildMemberAdd", (member) => {
-  const guild = member.guild;
-  if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
-  newUsers[guild.id].set(member.id, member.user);
-
-  if (newUsers[guild.id].size > 10) {
-    const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
-    guild.channels.find(channel => channel.name === "welcome-goodbye").send("Welcome our new users!\n" + userlist);
-    newUsers[guild.id].clear();
-  }
-});
-
-client.on("guildMemberRemove", (member) => {
-  const guild = member.guild;
-  if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
-});
+bot.on("guildMemberAdd", member => {
+  const channel = member.guild.channels
+    .find(ch => ch.name === "welcome-goodbye");
+  if (!channel) return;
+  channel.send(
+    `**Welcome To Server .....**`
+  );
+}); 
