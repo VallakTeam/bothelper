7cbd310b62f10c390cd5398adc2f8a3ec720e7c9
Glitch (verter-music) <none>
2020-01-02 23:20:48 +0000

    ⛪️👦 Checkpoint ./commands/welcome.js:6470734/3248 ./watch.jsons:6470734/2 ./commands/time.js:6470734/27

--- commands/time.js
+++ commands/time.js
@@ -9,7 +9,7 @@ module.exports.run = async (bot, message, args) => {
   .setAuthor(`${bot.user.tag}`, bot.user.displayAvatarURL)
   .setThumbnail(bot.user.displayAvatarURL)
     .addField("r.timer","Set a custome timer")
-   .addField("r.weather","Get custome weather stats. ``Ex:`` ?weather us")
+   .addField("r.cuaca","Get custome weather stats. ``Ex:`` r.cuaca us")
 .addField("r.remind","Set custome reminder. ``Ex:`` r.remind 1m to to school")
   .setFooter("If you found a bug please report it using ?bugreport")
     message.channel.send(embed);
