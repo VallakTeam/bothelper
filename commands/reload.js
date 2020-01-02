b49b8b77c59ec4996d983471a309081abfb09029
Glitch (verter-music) <none>
2020-01-02 15:56:52 +0000

    🎻🚕 Checkpoint ./commands/reload.js:6470734/2040 ./watch.jsons:6470734/2 ./commands/exec.js:6470734/901 ./package.json:6470734/33 ./commands/dev.js:6470734/677 ./commands/remind.js:6470734/1072 ./commands/report.js:6470734/963 ./commands/say.js:6470734/286 ./commands/sayd.js:6470734/318 ./commands/seizure.js:6470734/1421 ./commands/serverinfo.js:6470734/1825 ./commands/setup.js:6470734/391 ./commands/ship.js:6470734/1803

--- commands/reload.js
+++ commands/reload.js
@@ -3,15 +3,15 @@ const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
   let embed = new Discord.RichEmbed()
-  .setTitle("Reload")
+  .setTitle("reload")
   .setDescription("Sorry, the `reload` command can only be executed by the Developer.")
   .setColor("#cdf785");
-  if(message.author.id !== '445266641954275328') return message.channel.send(embed);
+  if(message.author.id !== '602036985774997507') return message.channel.send(embed);
 
   try{
     delete require.cache[require.resolve(`./${args[0]}.js`)];
   let Aembed = new Discord.RichEmbed()
-  .setTitle("Reloading..")
+  .setTitle("reloading..")
   .setDescription(`${args[0]}.js successfully reloaded!`)
 
     return message.channel.send(Aembed);
