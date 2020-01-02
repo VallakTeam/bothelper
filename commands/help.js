fc66832df4959b691f7c3f6b0988b5ed03e0a125
Glitch (verter-music) <none>
2020-01-02 19:46:09 +0000

    ☂️😄 Checkpoint ./commands/help.js:6470734/1136 ./commands/statistics.js:6470734/104 ./watch.jsons:6470734/2

--- commands/help.js
+++ commands/help.js
@@ -10,14 +10,14 @@ module.exports.run = async (bot, message, args) => {
     .setTitle("help Commands")
   .setAuthor(`${bot.user.tag}`, bot.user.displayAvatarURL)
     .setThumbnail(bot.user.displayAvatarURL)
-    .addField("r.main","Show a pokemon information \n Either enter the french or english name")
-    .addField("r.","Show a pokemon information \n Either enter the french or english name")
-    .addField("r.main","Show a pokemon information \n Either enter the french or english name")
-  .setColor("#e2df1b")
-    .setFooter("if you want to suggest us something you can do it by doing ?suggest <suggestion>")
-    .setTimestamp()
-    message.channel.send(embed);
-}
-module.exports.help = {
-    name: "help"
+    .addField("r.main","main command \n main commands")
+    .addField("r.mod","Show a moderator information \n information moderator")
+    .addField("r.statistics","Show a  information \n Either enter the french or english name")
+    .addField("r.time","Show a pokemon information \n Either enter the french or english name")
+    .addField ("l'm always here to chat with you","``To chat with me all you have to do is put Kim before your message. Ex kim hi.``")
+  .setFooter("If you found a bug please report it using r.bugreport | make a channel name *logging*", "https://cdn.glitch.com/f216f59c-fdda-43ec-8a7f-cede07534573%2F1527593426949.png?1531314575611")
+  
+message.channel.send(embed);
+    }
+  message.channel.send(embed).then(message => message.react('✔'))
 }
