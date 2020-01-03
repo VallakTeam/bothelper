9e2ce3acf0ca7e16ffb6e0de027d6fde8f077b28
Glitch (earthy-espadrille) <none>
2020-01-03 12:49:01 +0000

    🍼☔️ Checkpoint ./commands/mybot.js:6470734/100

--- commands/mybot.js
+++ commands/mybot.js
@@ -5,6 +5,7 @@ module.exports.run = async (bot, message, args) => {
   
   let msg =  bot.guilds.map(guild => `**${guild.name}** Members: ${guild.memberCount}`).join('\n');
   let embed = new Discord.RichEmbed()
+  let msg bot.guilds.map(guild => `**${guild.name}** 
   .setTitle(`I am in ${bot.guilds.size} guilds!`)
   .setDescription(`${msg}`)
   .setColor("#ebf442");
