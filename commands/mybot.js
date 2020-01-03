e05933a2ff79b4e56786b8a48cad75957abc9fdd
Glitch (earthy-espadrille) <none>
2020-01-03 23:36:08 +0000

    🌮📊 Checkpoint ./commands/mybot.js:6470734/926

--- commands/mybot.js
+++ commands/mybot.js
@@ -6,17 +6,19 @@ module.exports.run = async (bot, message, args) => {
   let msg =  bot.guilds.map(guild => `**${guild.name}** Members: ${guild.memberCount}`).join('\n');
   
   let embed = new Discord.RichEmbed()
-  .setTitle(`I am in ${bot.guilds.size} guilds!`)
+  .setTitle(`I am in ${bot.guilds.size} User!`)
   .setDescription(`${msg}`)
-  .addField("Server", bot.guilds.size)
-  .addField("Channel", bot.channels.size)
-  .addField("User", bot.users.size)
-  .addField("**Bot**", `${bot.user.tag}`)
   .setColor("RANDOM");
   message.channel.send(embed);
 }
 
+module.exports.run = async (bot, message, args) => {
 
-module.exports.help = {
-  name: "mybot"
+let msg =  bot.guilds.map(guild => `**${guild.name}** Server: ${bot.guilds.size.toLocaleString()}`).join('\n');
+  
+  let embed = new Discord.RichEmbed()
+  .setTitle(`I am in ${bot.guilds.size} Server!`)
+  .setDescription(`${msg}`)
+  .setColor("RANDOM");
+  message.channel.send(embed);
 }
