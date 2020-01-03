82336eddef95955beb569912166e2693c2f5b797
Glitch (earthy-espadrille) <none>
2020-01-03 17:43:34 +0000

    📈🤑 Checkpoint ./commands/invite.js:6470734/104 ./commands/help.js:6470734/73 ./watch.json:6470734/2

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ let commands = new Discord.RichEmbed()
   .setTimestamp()
   .setAuthor(`Help List Rakan Aditya™`, "https://cdn.discordapp.com/avatars/657351969253490699/3615dfbdff9e139478686acd04264ee6.png?size=2048")
   .setThumbnail("https://cdn.discordapp.com/avatars/657351969253490699/3615dfbdff9e139478686acd04264ee6.png?size=2048")
-.addField(`---- **help command** --- `,`\`r.main\`  \`r.mod\` \`r.time\` \`r.statistics\` \`r.jadwalsholat <kota>\` \`r.music\` \`r.dev\``)
+.addField(`---- **help command** --- `,`\`r.main\` \`r.mod\` \`r.time\` \`r.statistics\` \`r.jadwalsholat <kota>\``)
 .addField(`---- bot invite --- `,`[\`klik disini\`](https://discordapp.com/oauth2/authorize?client_id=657351969253490699&permissions=8&scope=bot)`)
 
 message.channel.send(commands).then(message => message.react('?'))
