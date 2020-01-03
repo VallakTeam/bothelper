3dc0bfb0dcad10bf66ac745c8a2a8abb7d1004a8
Glitch (verter-music) <none>
2020-01-03 01:19:46 +0000

    🚎🍵 Checkpoint ./commands/welcome.js:6470734/769

--- commands/welcome.js
+++ commands/welcome.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("guildMemberAdd", member => {
-  var channel = member.guild.channels.find(ch => ch.id === "")
+  var channel = member.guild.channels.find(ch => ch.id === "662433738981900338")
 
 if(!channel) return;
 
@@ -11,4 +11,25 @@ let embed = new Discord.RichEmbed()
 .setDescription(`
 Your Identify
 \`\`\`Username : ${member.user.username}
-Tag : $
+Tag : ${member.user.tag}
+Discrim : ${member.user.discriminator}
+Member now : {member.guild.memberCount}\`\`\``)
+  .setColor("RANDOM")
+  if(channel) return channel.send(embed)
+});
+client.on("guildMemberRemove", member => {
+  var channel = member.guild.channels.find(ch => ch.id === "662433738981900338")
+
+if(!channel) return;
+
+let embed = new Discord.RichEmbed()
+.setTitle("Goodbye to my frends!")
+.setDescription(`
+Your Identify
+\`\`\`Username : ${member.user.username}
+Tag : ${member.user.tag}
+Discrim : ${member.user.discriminator}
+Member now : {member.guild.memberCount}\`\`\``)
+  .setC
+  if(channel) return channel.send(embed)
+});
