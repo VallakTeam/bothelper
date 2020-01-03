00992dccc6d40ed99b50789a4e49cf71ccb943a5
Glitch (earthy-espadrille) <none>
2020-01-03 11:38:04 +0000

    🎒🗃 Checkpoint ./welcome.js:6470734/1858 ./commands/welcome.js:6470734/3466 ./watch.jsons:6470734/2 ./index.js:6470734/3382

---
+++ commands/welcome.js
@@ -0,0 +1,51 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const http = require("https");
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+client.on("guildMemberAdd", member => {
+  var channel = member.guild.channels.find(ch => ch.id === "662433738981900338")
+
+if(!channel) return;
+
+let embed = new Discord.RichEmbed()
+.setTitle("Welcome to my frends!")
+.setDescription(`
+Your Identify
+\`\`\`Username : ${member.user.username}
+Tag : ${member.user.tag}
+Discrim : ${member.user.discriminator}
+Member now : ${member.guild.memberCount}\`\`\``)
+.setThumbnail("https://cdn.discordapp.com/avatars/657351969253490699/3615dfbdff9e139478686acd04264ee6.png?size=2048")
+.addField(`---- (**Dear Member**) --- `,`\`rame kan discord ini :v\``)
+  .setColor("RANDOM")
+  if(channel) return channel.send(embed)
+});
+client.on("guildMemberRemove", member => {
+  var channel = member.guild.channels.find(ch => ch.id === "662433738981900338")
+
+if(!channel) return;
+
+let embed = new Discord.RichEmbed()
+.setTitle("Goodbye to my frends!")
+.setDescription(`
+Your Identify
+\`\`\`Username : ${member.user.username}
+Tag : ${member.user.tag}
+Discrim : ${member.user.discriminator}
+Member now : ${member.guild.memberCount}\`\`\``)
+.setThumbnail("https://cdn.discordapp.com/avatars/657351969253490699/3615dfbdff9e139478686acd04264ee6.png?size=2048")
+.addField(`---- (**Dear Member**) --- `,`\`maaf discord ini blom tentu rame:(\``)
+  .setColor("RANDOM")
+  if(channel) return channel.send(embed)
+});
+client.login("NjU3MzUxOTY5MjUzNDkwNjk5.Xgp17w.nyWBwlqGOAwSAmKHcgPmBibGByM")
