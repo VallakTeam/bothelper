10b88593d1ae6b1f8e852219c9cc09d2a98cda83
Glitch (earthy-espadrille) <none>
2020-01-03 22:25:15 +0000

    😎🏢 Checkpoint ./commands/cmd.js:6470734/3505 ./commands/cmdavatar.js:6470734/1446

---
+++ commands/cmd.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+let commands = new Discord.RichEmbed().setColor("#4290AE")
+  .setTimestamp()
+  .setAuthor(`Help Cmd Rakan Aditya™`, "https://cdn.discordapp.com/avatars/657351969253490699/3615dfbdff9e139478686acd04264ee6.png?size=2048")
+  .setThumbnail("https://cdn.discordapp.com/avatars/657351969253490699/3615dfbdff9e139478686acd04264ee6.png?size=2048")
+  .addField(`---- **help command** --- `,`\`r.main\` \`r.mod\` \`r.time\` \`r.statistics\` \`r.jadwalsholat <kota>\``)
+message.channel.send(commands)
+
+}
