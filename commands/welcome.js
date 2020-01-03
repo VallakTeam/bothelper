b0bbd0e85cc4ae8bb2f6885230e07f040527f819
Glitch (verter-music) <none>
2020-01-03 01:07:46 +0000

    👌🎗 Checkpoint ./commands/welcome.js:6470734/776

--- commands/welcome.js
+++ commands/welcome.js
@@ -1,22 +1,14 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-bot.on("guildMemberAdd", member => {
-  const channel = member.guild.channels
-    .find(ch => ch.id === "662433738981900338");
-  if (!channel) return;
-  channel.send(
-    `**Welcome To Server .....**`
-  );
-}); 
+client.on("guildMemberAdd", member => {
+  var channel = member.guild.channels.find(ch => ch.id === "")
 
-bot.on("guildMemberAdd", member => {
-  const channel = member.guild.channels
-    .find(ch => ch.name === "welcome-goodbye");
-  if (!channel) return;
-  channel.send(
-    `**Welcome To Server .....**`
-  );
-});
+if(!channel) return;
 
-client.login("NjU3MzUxOTY5MjUzNDkwNjk5.Xgp17w.nyWBwlqGOAwSAmKHcgPmBibGByM")
+let embed = new Discord.RichEmbed()
+.setTitle("Welcome to my frends!")
+.setDescription(`
+Your Identify
+\`\`\`Username : ${member.user.username}
+Tag : $
