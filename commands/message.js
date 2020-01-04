ea58a2d1999efbdd7d8bd264f678cd04a97087ae
Glitch (earthy-espadrille) <none>
2020-01-04 22:42:03 +0000

    😼🍵 Checkpoint ./commands/message.js:6470734/955

--- commands/message.js
+++ commands/message.js
@@ -18,4 +18,28 @@ module.exports = async (client, message) => {
 
 
 
-  
+  const data = await client.db.get(`SELECT * FROM xpsytem WHERE guildid = "${message.guild.id}" AND userid = "${message.author.id}"`);
+
+
+const genXp = Math.floor(Math.random() * 15);
+
+
+if (!data) await client.db.run(`INSERT INTO xpsystem (guildid, userid, level, xp, totalxp) VALUES (?, ?, ?, ?, ?,`, [
+    message.guild.id,
+    message.author.id,
+    1,
+    genXp,
+    genXp
+]);
+else {
+    const requiredXp = data.level * 40;
+
+    
+    await client.db.run(`UPDATE xpsystem SET xp = ${data.xp + gen Xp}, totalxp = ${data.totalexp + genXp} WHERE guildid = "${message.guild.id}" And`)
+
+    if (data.xp + genXp >= requiredXp) {
+        message.reply(`Congratulations! You are now level **${data.level + 1} **!`);
+        await client.db.run(`UPDATE xpsystem SET level = ${data.level + 1}, xp = 0 WHERE guildid = "${message.guild.id  }" AND userid = "${message.author.id}"`);
+
+    }
+}
