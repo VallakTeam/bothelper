9ec5705040111485d7c388a72b52e10c28058aad
Glitch (earthy-espadrille) <none>
2020-01-04 19:25:01 +0000

    ❣️🏙 Checkpoint ./commands/pay.js:6470734/1042

--- commands/pay.js
+++ commands/pay.js
@@ -6,33 +6,11 @@ exports.run = async (client, message, args, tools) => {
 
 
     if (!message.mentions.members.first()) return message.channel.send(`**Please mention a user!**`);
-
-
-    let targetMember = message.mentions.member.first(),
-        amount = parseInt(args.join(' ').replace(targetMember, ''));
-
-
-
-    if (isNaN(amount)) return message.channel.send(`**Please define an amount!**`);
-
-
-    let targetBalance = await db.fetch(`userBalance_${targetMember.id}`),
-        selfBalance = await db.fetch(`userBalance_${targetMember.id}`);
-
-
-    if (targetBalance === null) targetBalance = 0;
-    if (selfBalance === null) selfBalance = 0;
-
-
-    if (amount > selfBalance) return message.channel.send(`**Sorry, you don't have enought money!**`);
-
-
-    db.add(`userBalance_${targetMember.id}`, amount);
-    db.subtract(`userBalance_${message.author.id}`, amount);
-
-
-    message.channel.send(`**Successfully send{amount} to ${targetMember.user.tag}!$$`);
-
     
+    
+    let targetMember = message.mentions.members.first(),
+        amount = parseInt(args.join(' ').replace(targetMember,``));
 
-}
+
+
+    if (isNaN)(amount)) return 
