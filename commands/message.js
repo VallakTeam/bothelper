1c8664845ef9624cfe72c9c57a833c3a5df9aced
Glitch (earthy-espadrille) <none>
2020-01-04 22:07:06 +0000

    😻🌌 Checkpoint ./commands/message.js:6470734/477

--- commands/message.js
+++ commands/message.js
@@ -4,3 +4,18 @@ const { owners } = require("../config");
 
 module.exports = async (client, message) => {
 
+    if (!client.prefix[message.guild.id]) {
+        const data = await client.db.get(`SELECT * FROM server WHERE guildid = "${message}"`);
+
+
+        client.prefix[message.guild.id] = data ? data.prefix : client.prefix["default"];
+
+   }
+
+
+
+
+
+
+
+  if (!data) await client
