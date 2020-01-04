d891a2b5c7f0efac8a14c6dbb108420b51ba97d5
Glitch (earthy-espadrille) <none>
2020-01-04 00:00:07 +0000

    🚴🗾 Checkpoint ./commands/mybot.js:6470734/5146

--- commands/mybot.js
+++ commands/mybot.js
@@ -3,21 +3,20 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
   
- let msg =  bot.guilds.map(guild => `**${bot.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()};
-  let embed = new Discord.RichEmbed()
-  .setTitle(``I am in ${bot.guilds.size} User!``)
-  .setDescription(``${msg}``)
-  .setColor("RANDOM");
-  message.channel.send(embed);
-
-module.exports.run = async (bot, message, args) => {
-
-let msg =  bot.guilds.map(guild => `**${guild.name}** Server: ${bot.guilds.size.toLocaleString()}`).join('\n');
+  let msg =  bot.guilds.map(guild => `**${guild.name}** Members: ${guild.memberCount}`).join('\n');
   
   let embed = new Discord.RichEmbed()
-  .setTitle(`I am in ${bot.guilds.size} Server!`)
+  .setTitle(`I am in ${bot.guilds.size} guilds!`)
   .setDescription(`${msg}`)
+  .addField("Server", bot.guilds.size)
+  .addField("Channel", bot.channels.size)
+  .addField("User", bot.users.size)
+  .addField("**Bot**", `${bot.user.tag}`)
   .setColor("RANDOM");
   message.channel.send(embed);
 }
+
+
+module.exports.help = {
+  name: "mybot"
 }
