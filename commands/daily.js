b74b04ebfc548f549afb62f10340848d70f36284
Glitch (earthy-espadrille) <none>
2020-01-06 08:27:04 +0000

    🎐💍 Checkpoint ./commands/daily.js:6470734/48:8308650/466 ./watch.json:8308650/8

--- commands/daily.js
+++ commands/daily.js
@@ -5,20 +5,22 @@ const ms = require('parse-ms')
 exports.run = async (client, message, args) => {
 
     let timeout = 86400000
-    let amount = 250
-
-    let daily = await db.fetch(`userBalance_${message.guild.id}_${message.author.id}`);
+    let amount = Math.floor(Math.random() * 500) + 1;
+    
+    let daily = await db.fetch(`userBalance_${message.author.id}`);
 
     if (daily !== null && timeout - (Date.now() - daily) > 0) {
         let time = ms(timeout - (Date.now() - daily));
 
         message.channel.send(`Anda telah mengumpulkan hadiah harian Anda, Anda dapat kembali dan mengambilnya **${time.hours}h ${time.minutes}m ${time.seconds}s**!`)
     } else {
-        
-        message.channel.send(`Successfully collected $${amount}`);
-      
-        db.add(`userBalance_${message.author.id}`, 250)
-        db.set(`lastDaily_${message.author.id}`, Date.now())
+    let embed = new Discord.RichEmbed()
+    .setAuthor(`Daily`, message.author.displayAvatarURL)
+    .setColor("GREEN")
+    .setDescription(`**Harian Reward**`)
+    .addField(`Collected`, amount)
+        message.channel.send(embed)
+        db.add(`userBalance_${message.author.id}`, amount)
+        db.set(`daily_${message.author.id}`, Date.now())
     }
-
 }
