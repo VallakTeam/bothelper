6f3dae950b0beb8c3c8c9cc2d276984939e6594b
Glitch (earthy-espadrille) <none>
2020-01-07 07:29:08 +0000

    🚶🐀 Checkpoint ./commands/help.js:6470734/26 ./watch.json:6470734/8 ./commands/cmd.js:6470734/81

--- commands/cmd.js
+++ commands/cmd.js
@@ -6,7 +6,7 @@ let commands = new Discord.RichEmbed().setColor("#4290AE")
   .setTimestamp()
   .setAuthor(`Help Cmd Rakan Aditya™`, "https://cdn.discordapp.com/avatars/657351969253490699/3615dfbdff9e139478686acd04264ee6.png?size=2048")
   .setThumbnail("https://cdn.discordapp.com/avatars/657351969253490699/3615dfbdff9e139478686acd04264ee6.png?size=2048")
-  .addField(`---- **help command r.cmd** --- `,`\`r.cmdavatar\` \`r.cmdbotstats\` \`r.cmdinstagram\` \`r.statistics\` \`r.jadwalsholat <kota>\``)
-message.channel.send(commands)
+  .addField(`---- **help command r.cmd** --- `,`\`r.cmdavatar\` \`r.cmdbotstats\` \`r.cmdinstagram\``)
+message.channel.send(commands).then(message => message.react('♨'))
 
 }
