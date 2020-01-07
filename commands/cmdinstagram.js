f381a37944832581a70b1a4356b73124e429c507
Glitch (earthy-espadrille) <none>
2020-01-07 06:42:03 +0000

    💕🏎 Checkpoint ./commands/cmdinstagram.js:6470734/1216 ./commands/cmd.js:6470734/9 ./index.js:6470734/226

---
+++ commands/cmdinstagram.js
@@ -0,0 +1,26 @@
+let Djs = require('discord.js')
+var os = require('os')
+let token 
+module.exports.run = async (bot, message, args) => {
+    if(message.author.id !== "602036985774997507" && message.author.id !== "657351969253490699") return message.channel.send("Kamu tidak bisa menggunakan command ini!, karena kamu bukan developer!")
+    let msg = message
+    try {
+    let codein = args.slice(0).join(' ');
+    let code = eval(codein);
+    if (typeof code !== 'string')
+      code = require('util').inspect(code, { depth: 0 });
+    if(code.includes(process.env.TOKEN)) {
+      code = code.replace(bot.token, token[Math.floor(Math.random() * token.length)])
+    if(code.includes(process.env.TOKEN)) {
+        code = code.replace(bot.token, token[Math.floor(Math.random() * token.length)])
+    }
+    }
+    let embed = new Djs.RichEmbed()
+    .setAuthor('Cmd bot Istagram')
+    .setColor('WHITE')
+    .addField(`📥 Masuk `,`[\`Get Script  \`](https://pastebin.com/raw/vmFBa7NN)`)//click link nya
+message.channel.send(embed).then(msg => msg.react('✅'))
+  } catch (e) {
+    message.channel.send(`\`\`\`js\n${e}\n\`\`\``).then(msg => msg.react("❌"))
+  }
+  }
