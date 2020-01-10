9001f269e0f08d4b4a1cbfb18815ea2254d63793
Glitch (earthy-espadrille) <none>
2020-01-10 16:52:54 +0000

    🌹🚛 Checkpoint ./commands/invite.js:6470734/2329 ./watch.json:6470734/4

--- commands/invite.js
+++ commands/invite.js
@@ -1,10 +1,9 @@
 const Discord = require('discord.js')
 module.exports.run = async (bot, message, args) => {
   let embed = new Discord.RichEmbed()
-    .setTitle("Invite link:")
-    .setDescription("[Invite me!](https://discordapp.com/oauth2/authorize?client_id=661648558696562698&perm")
-    
+    .setTitle("Invite link bot:")
+    .setDescription("[Invite me!](https://discordapp.com/oauth2/authorize?client_id=661648558696562698&permissions=8&scope=bot)")
+    .setThumbnail("https://cdn.discordapp.com/avatars/661648558696562698/d5949dedc6bd8eb2ab9ab324c6e9cd1b.png?size=2048")
+    .setColor("RANDOM")
     message.channel.send(embed)       
 }
-
-//invite wkwk copas apaan yang mana lu :v mana ada ku copas bjir
