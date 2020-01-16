f478513f58759508d4b26b6c144bb6349af0c9f9
Glitch (gossamer-cattle) <none>
2020-01-16 18:03:20 +0000

    👽🍿 Checkpoint ./commands/help.js.js:6470734/58 ./watch.json:6470734/4

--- commands/help.js.js
+++ commands/help.js.js
@@ -2,17 +2,13 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 let embed = new Discord.RichEmbed()
-let commands = new Discord.RichEmbed()
-  
   .setColor("#4290AE")
   .setTimestamp()
   .setAuthor(`Help List Hack PUBG™`, "https://cdn.discordapp.com/avatars/666975140092837909/97b7199102b9a355b27a8c9e38575834.png?size=2048")
   .setThumbnail("https://cdn.discordapp.com/avatars/666975140092837909/97b7199102b9a355b27a8c9e38575834.png?size=2048")
 .addField(`---- **help command ** --- `,`\`~aio\` \`~bpsinki\` \`~driver\` \`~kingsmen\` \`~reymen\` \`~sinki\` \`~temp\``)
-
-
 message.author.send({embed});
-  message.reply ("l just send you all the **__help__** commands in your DMs 📥")
+  message.reply ("l just send you **help** commands in your DMs 📥")
 }
 
 module.exports.help = {
