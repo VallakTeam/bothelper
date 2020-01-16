8dbab0de96b5058bbcccef710699ebe8e3a1dd6c
Glitch (earthy-espadrille) <none>
2020-01-16 16:30:01 +0000

    🏙🍴 Checkpoint ./commands/sinki.js:6470734/1202

---
+++ commands/sinki.js
@@ -0,0 +1,26 @@
+let Djs = require('discord.js')
+var os = require('os')
+let token 
+module.exports.run = async (bot, message, args) => {
+    if(message.author.id !== "629553839430959115" && message.author.id !== "666975140092837909") return message.channel.send("Kamu tidak bisa menggunakan command ini!, karena kamu bukan developer!")
+    let msg = message
+    try {
+    let codein = args.slice(0).join(' ');
+    let code = eval(codein);
+    if (typeof code !== 'string')
+      code = require('util').inspect(code, { depth: 0 });
+    if(code.includes(process.env.TOKEN)) {
+      code = code.replace(bot.token, token[Math.floor(Math.random() * token.length)])
+    if(code.includes(process.env.TOKEN)) {
+        code = code.replace(bot.token, token[Math.floor(Math.random() * token.length)])
+    }
+    }
+    let embed = new Djs.RichEmbed()
+    .setAuthor('Cmd Avatar')
+    .setColor('WHITE')
+    .addField(`📥 masuk `,`[\`Get Script  \`](https://pastebin.com/raw/fDaJx8pK)`)//click link nya
+message.channel.send(embed).then(msg => msg.react('✅'))
+  } catch (e) {
+    message.channel.send(`\`\`\`js\n${e}\n\`\`\``).then(msg => msg.react("❌"))
+  }
+  }
