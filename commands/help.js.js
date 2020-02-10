68d204f6cda23156981b52f5354eb916202f7b28
Glitch (gossamer-cattle) <none>
2020-02-10 16:34:31 +0000

    🔦💿 Checkpoint ./commands/help.js.js:6629094/10 ./watch.json:6629094/2

--- commands/help.js.js
+++ commands/help.js.js
@@ -8,7 +8,7 @@ let embed = new Discord.RichEmbed()
   .setFooter(`Requested by ${message.author.username}`, message.author.displayAvatarURL)
   .setThumbnail(`${message.author.avatarURL}`)
 .setTitle('𝐇𝐄𝐋𝐏 𝐂𝐎𝐌𝐌𝐀𝐍𝐃')
-.addField(".", "$sinki | Link Download SINKI New V1.27\n$bpsinki | Link Download Bypass SINKI V1.9\n$aio | Link Download SINKI All In One Tool V1.5\n$bpbeast | Link Download Beast Bypass\n$reymen | link download reymen\n$driver | Link Download Full Driver\n$temp | Link Download Auto Clear Temp")
+.addField(".", "$sinki | Link Download SINKI New V2.10\n$bpsinki | Link Download Bypass SINKI V1.9\n$aio | Link Download SINKI All In One Tool V1.5\n$bpbeast | Link Download Beast Bypass\n$reymen | link download reymen\n$driver | Link Download Full Driver\n$temp | Link Download Auto Clear Temp")
 message.author.send({embed});
   message.react("✅");
   message.delete(2000).catch(console.error);//delete 3000 aja
