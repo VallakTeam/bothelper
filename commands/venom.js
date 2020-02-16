782f7e6fd53ca96834f0a4376dce8936003239cd
Glitch (gossamer-cattle) <none>
2020-02-16 18:41:02 +0000

    🛰🎡 Checkpoint ./commands/venom.js:6958328/102 ./watch.json:6958328/2

--- commands/venom.js
+++ commands/venom.js
@@ -17,7 +17,7 @@ module.exports.run = async (bot, message, args, color, queue) => {
     let embed = new Djs.RichEmbed()
      .setFooter(`Requested by ${message.author.username}`, message.author.displayAvatarURL)
      .setColor('WHITE')
-     .addField  ("**𝐕𝐄𝐍𝐎𝐌 𝐏𝐑𝐎**",`[\`HERE DOWLOAD\`](https://mega.nz/#F!1QFS1KqA!6HVjnxmr9SHHzNZ6PHxXRA)`)//link dowload nya samain dnegan reyman ganti https
+     .addField  ("**𝐕𝐄𝐍𝐎𝐌 𝐏𝐑𝐎**",`[\`HERE DOWLOAD\`](https://mega.nz/#F!TxN3ACZQ!7YgLtp_JkDg-3cAterrj_w)`)//link dowload nya samain dnegan reyman ganti https
      .addField  ("**𝐎𝐔𝐑 𝐘𝐎𝐔𝐓𝐔𝐁𝐄**",`[\`𝐍𝐄𝐖 𝐘𝐎𝐔𝐓𝐔𝐁𝐄\`](https://www.youtube.com/channel/UC3WbAx_l7HtseSWpxRYI6rw)`)//link server
 message.react("✅");
   message.delete(2000).catch(console.error);
