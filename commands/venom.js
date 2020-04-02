0162984631d28be16d2e7edfc150092335a44495
Glitch (gossamer-cattle) <none>
2020-04-02 20:43:30 +0000

    👙🎊 Checkpoint ./commands/venom.js:13834785/2403 ./watch.json:13834785/2

--- commands/venom.js
+++ commands/venom.js
@@ -16,11 +16,12 @@ module.exports.run = async (bot, message, args, color, queue) => {
     }
     let embed = new Djs.RichEmbed()
      .setFooter(`Requested by ${message.author.username}`, message.author.displayAvatarURL)
-     .setColor('WHITE')
-     .addField  ("**𝐕𝐄𝐍𝐎𝐌 𝐀𝐍𝐓𝐈 𝐁𝐀𝐍**",`[\`HERE DOWLOAD\`](http://venombest.com/download)`)//link dowload aioinki ganti mediafire nya
+      .addField  ("**𝐕𝐄𝐍𝐎𝐌 𝐀𝐍𝐓𝐈 𝐁𝐀𝐍**",`[\`HERE DOWLOAD\`](http://venombest.com/download)`)//link dowload aioinki ganti mediafire nya
      .addField  ("**𝐎𝐔𝐑 𝐘𝐎𝐔𝐓𝐔𝐁𝐄**",`[\`𝐍𝐄𝐖 𝐘𝐎𝐔𝐓𝐔𝐁𝐄\`](https://rebrand.ly/4e918)`)//link server
-message.author.send(embed).then(msg => msg.react('✅'))
+message.react("✅");
+  message.delete(2000).catch(console.error);
+    message.author.send(embed).then(msg => msg.react('✅'))
   } catch (e) {
-    message.channel.send(`\`\`\`js\n${e}\n\`\`\``).then(msg => msg.react("❌"))//reaciton
+    message.author.send(`\`\`\`js\n${e}\n\`\`\``).then(msg => msg.react("❌"))//reaciton
   }
   }
